fd7ce32984867b63cae7aef78281cdd64eb345b0
Aleksandr Apostolov <61287091+apostolovleks@users.noreply.github.com>
2023-04-26 18:02:44 +0000

    some changes

--- Start/Ch_1/challenge_start.py
+++ Start/Ch_1/challenge_start.py
@@ -11,6 +11,50 @@ import json
 # 4: Print the top 10 most significant events, with the significance value of each
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open(
+    "/workspaces/advanced-python-working-with-data-4312001/30DayQuakes.json", "r"
+) as datafile:
     data = json.load(datafile)
-    
+
+
+# 1: How many quakes are there in total?
+total_quakes = len(data["features"])
+print(f"Total quakes: {total_quakes}")
+
+
+# 2: How many quakes were felt by at least 100 people?
+total_quakes_felt_by_100_people = sum(
+    quake["properties"]["felt"] is not None and quake["properties"]["felt"] >= 100
+    for quake in data["features"]
+)
+print(f"Total quakes felt by at least 100 poeple: {total_quakes_felt_by_100_people}")
+
+
+# 3: Print the name of the place whose quake was felt by the most people, with the # of reports
+most_felt_reposrts = max(
+    [
+        quake["properties"]["felt"]
+        for quake in data["features"]
+        if quake["properties"]["felt"] is not None
+    ]
+)
+print(f"Most felt reposrts: {most_felt_reposrts}")
+
+
+# 4: Print the top 10 most significant events, with the significance value of each
+def sorting_by_significance(events):
+    significant = events["properties"]["sig"]
+    if events["properties"]["sig"] is None:
+        significant = 0
+    return float(significant)
+
+
+data["features"].sort(key=sorting_by_significance, reverse=True)
+
+
+print(f"Top 10 most significant events were:\n")
+
+for event in data["features"][:10]:
+    print(
+        f"Event: {event['properties']['mag']} - {event['properties']['place']}, Significance: {event['properties']['sig']}"
+    )
