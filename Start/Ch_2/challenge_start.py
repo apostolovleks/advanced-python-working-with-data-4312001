e0ff84bff99e93ece1d20e36feb8ad1d0a3b4a9e
Aleksandr Apostolov <61287091+apostolovleks@users.noreply.github.com>
2023-04-27 09:58:58 +0000

    Ch_2

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,21 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
+from collections import Counter
 
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open(
+    "/workspaces/advanced-python-working-with-data-4312001/30DayQuakes.json", "r"
+) as datafile:
     data = json.load(datafile)
+all_vent_type = Counter(
+    [
+        quake["properties"]["type"]
+        for quake in data["features"]
+        if quake["properties"]["type"] is not None
+    ]
+)
+
+for event_type, quantity in all_vent_type.items():
+    print(f"{event_type:<15} : {quantity}")
