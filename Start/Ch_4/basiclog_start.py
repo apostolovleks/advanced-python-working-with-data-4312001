42b8bf6fc54f10ec450aa1e2f1dde542c9c258bb
Aleksandr Apostolov <61287091+apostolovleks@users.noreply.github.com>
2023-05-01 18:32:59 +0000

    Ch_4

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,24 @@
 # demonstrate the logging api in Python
 
 # TODO: use the built-in logging module
+import logging
 
 
 # TODO: Use basicConfig to configure logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="/workspaces/advanced-python-working-with-data-4312001/Start/Ch_4/output.log",
+    filemode="w",
+)
 
 # TODO: Try out each of the log levels
+logging.debug("Debug message")
+logging.info("Info message")
+logging.error("error message")
+logging.warning("warning message")
+logging.critical("critical message")
 
 # TODO: Output formatted strings to the log
-
+x = "string"
+y = 10
+logging.info(f"Here is a {x} variavble and int: {y}")
--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -2,14 +2,24 @@
 
 import logging
 
+
 # TODO: add another function to log from
+def func():
+    logging.debug("This is a debug-level message", extra=extdata)
 
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+fmstr = "User: %(user)s %(asctime)s: %(levelname)s: %(funcName)s Line: %(lineno)d %(message)s"
+datestr = "%d/%m/%Y %H:%M:%S %p"
+extdata = {"user": "John Doe"}
+logging.basicConfig(
+    filename="/workspaces/advanced-python-working-with-data-4312001/Start/Ch_4/output.log",
+    level=logging.DEBUG,
+    format=fmstr,
+    datefmt=datestr,
+)
 
-logging.info("This is an info-level log message")
-logging.warning("This is a warning-level message")
-
+logging.info("This is an info-level log message", extra=extdata)
+logging.warning("This is a warning-level message", extra=extdata)
+func()
